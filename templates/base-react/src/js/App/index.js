634f10ba2905d44bc6169dbf43665147368a9045
Kaustav Chakraborty <kaustav.ron@gmail.com>
2019-05-15 22:10:26 +0530

    Kaustav | #11 | Added prettier for base-react application | TG-11

--- templates/base-react/src/js/App/index.js
+++ templates/base-react/src/js/App/index.js
@@ -1,10 +1,10 @@
 import React, { PureComponent } from 'react';
-import "./index.scss";
+import './index.scss';
 
 export default class App extends PureComponent {
     render() {
         return (
-            <div className="app__wrapper">Base App Created.</div>
+            <div className='app__wrapper'>Base App Created.</div>
         );
     }
 }
