76c3f55fce16b33f4376a4eb2d906c1dfe6990c3
Kaustav Chakraborty <kaustav.ron@gmail.com>
2019-05-16 00:19:41 +0530

    Kaustav | #8 | Configured ESLint for base react app project. Fixed lint issues. | TG-8

--- templates/base-react/src/js/App/index.js
+++ templates/base-react/src/js/App/index.js
@@ -2,9 +2,9 @@ import React, { PureComponent } from 'react';
 import './index.scss';
 
 export default class App extends PureComponent {
-    render() {
-        return (
-            <div className='app__wrapper'>Base App Created.</div>
-        );
-    }
+  render() {
+    return (
+      <div className="app__wrapper">Base App Created.</div>
+    );
+  }
 }
