e075fb0b80f4dce114e45e65d96517aef88b7526
Kaustav Chakraborty <kaustav.ron@gmail.com>
2019-05-16 00:56:12 +0530

    Kaustav | #9 | Added shallow to test globals. Modified eslint. | TG-9

--- templates/base-react/src/setupTests.js
+++ templates/base-react/src/setupTests.js
@@ -1,4 +1,5 @@
-import { configure } from 'enzyme';
+import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
+global.shallow = shallow;
 configure({ adapter: new Adapter() });
